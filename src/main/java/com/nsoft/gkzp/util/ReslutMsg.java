a05bd8725b9a47cd29085cd941033b97d2f25f01
yangzhandong <yang1986jl@163.com>
2019-09-29 10:37:20 +0800

    返回结果类 规范修正

--- src/main/java/com/nsoft/gkzp/util/ReslutMsg.java
+++ src/main/java/com/nsoft/gkzp/util/ReslutMsg.java
@@ -1,20 +1,25 @@
 package com.nsoft.gkzp.util;
 
 import org.springframework.stereotype.Component;
-
+/**公共方法
+ * 返回结果信息
+ * @author 孙理锟
+ * @date 2019.09.29
+ */
 @Component
 public class ReslutMsg {
-    int type = MsgType.NONE;;
-    String msg = "";
+
+    int type    = MsgType.NONE; //返回信息类型
+    String msg  = "";           //返回信息
 
     /**
      * 业务提示信息类型
      *
      */
     public static class MsgType {
-        final public static int NONE  = 0;
-        final public static int INFO  = 1;
-        final public static int ERROR = 2;
+        final public static int NONE  = 0; //无
+        final public static int INFO  = 1;//正常
+        final public static int ERROR = 2;//问题
     }
 
     public int getType() {
