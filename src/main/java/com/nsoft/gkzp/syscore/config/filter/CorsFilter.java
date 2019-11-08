8c297e28fa5e7d696294acf62366a6cd9e1b56f8
yangzhandong <yang1986jl@163.com>
2019-11-08 11:29:31 +0800

    跨域白名单再再重新处理下

--- src/main/java/com/nsoft/gkzp/syscore/config/filter/CorsFilter.java
+++ src/main/java/com/nsoft/gkzp/syscore/config/filter/CorsFilter.java
@@ -4,8 +4,7 @@ import com.nsoft.gkzp.syscore.config.MyDefinedUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -14,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebFilter(urlPatterns = "/*", filterName = "corsFilter")
-@PropertySource(value="classpath:application.properties")
+
 public class CorsFilter implements Filter {
 
     final private static Logger logger = LogManager.getLogger(CorsFilter.class);
@@ -26,9 +25,6 @@ public class CorsFilter implements Filter {
     public void destroy() {
     }
 
-    //读取application.properties配置文件配置的http访问端口号
-    @Value("${server.port}")
-    public  int SYSTEM_HTTPS_PORT;
     /**
      * 此过滤器只是处理跨域问题
      * @param servletRequest
@@ -58,15 +54,21 @@ public class CorsFilter implements Filter {
         //允许跨域白名单
         String whiteList=myDefinedUtil.SYSTEM_ACCESSCONTROLALLOWORIGIN;
         boolean isValid = false;
+         String temp    = null;
         if(origin != null){
-                int  a = origin.indexOf("://")+3;
-                int  b = origin.indexOf(":"+SYSTEM_HTTPS_PORT);
-                if(b<0){  //如果需要跨域的是80端口,则origin不会带上端口号（eg：前端地址栏为http://127.0.0.1,即前端用的是80端口号。访问后端，这里的origin=http://127.0.0.1 即b=-1，故这里需特殊处理）
-                    b = origin.length();
+            try {
+                int a = origin.indexOf("://") + 3;
+                temp = origin.substring(origin.indexOf("://") + 3);
+                int b = temp.indexOf(":");
+                if (b > 0) {
+                    temp = temp.substring(0, b);
                 }
-            isValid = whiteList.contains(origin.substring(a,b)); //将origin截出ip字符串
+                isValid = whiteList.contains(temp); //将origin截出ip字符串
+            }catch (Exception e){
+                logger.error("白名单校验出错:"+e.getMessage(),e);
+            }
         }
-        logger.info("跨域验证:origin="+origin+";;;;;;isValid="+isValid);// 如为跨域请求，下面的"Access-Control-Allow-Origin"值置为null，就无法访问了。。。如果为非跨域请求，这个为null不会受影响，依然允许访问
+        logger.info("跨域验证:origin="+origin+"***temp="+temp+"***isValid="+isValid);// 如为跨域请求，下面的"Access-Control-Allow-Origin"值置为null，就无法访问了。。。如果为非跨域请求，这个为null不会受影响，依然允许访问
         response.setHeader("Access-Control-Allow-Origin", isValid ? origin : "null");// 允许指定域访问跨域资源(这里不能写*，*代表接受所有域名访问，如写*则下面一行代码无效。谨记)
         response.setHeader("Access-Control-Allow-Credentials", "true");//true代表允许客户端携带cookie(此时origin值不能为“*”，只能为指定单一域名)
         response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH"); /// 允许浏览器在预检请求成功之后发送的实际请求方法名
