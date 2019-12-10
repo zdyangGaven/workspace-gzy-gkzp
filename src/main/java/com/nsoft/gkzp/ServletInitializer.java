c54480217430b595d48c9bdf0b5649ae4332908c
yangzhandong <yang1986jl@163.com>
2019-12-10 14:42:17 +0800

    添加注释

--- src/main/java/com/nsoft/gkzp/ServletInitializer.java
+++ src/main/java/com/nsoft/gkzp/ServletInitializer.java
@@ -1,4 +1,7 @@
 package com.nsoft.gkzp;
+/**
+ * 如果使用外置tomcat，此为外置tomcat的启动类
+ */
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +10,8 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(GzyGkzpApplication.class);
+        //传入SpringBoot应用的主程序
+        return application.sources(com.nsoft.gkzp.GzyGkzpApplication.class);
     }
 
 }
